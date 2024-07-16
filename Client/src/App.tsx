f4782ef19da2a767d5f66cff7a32103a60b13d3f
Mykolas Perevicius <mykolas.perevicius@projectinnovatenewark.org>
2024-07-16 14:44:05 -0400

    Fixed alignment issue by implementing Tailwind

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="flex justify-center items-center h-sc">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
