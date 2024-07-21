b8699fc81f4cd28399de996b6f9c75421c4aa794
Dalar-Coin <77028982+Dalar-Coin@users.noreply.github.com>
2024-07-20 22:40:51 -0400

    took sample api calls from tutorial

--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
 const app = express();
 const port = 3000;
 
@@ -8,6 +10,25 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://DalarCoin:Dennis625@eartrainrdb.o8i2rop.mongodb.net/EarTrainr?retryWrites=true&w=majority&appName=EarTrainrDB"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
